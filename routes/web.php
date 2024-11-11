3ed1f42d7e856c36f12e19835e1e29e4208dce6b
Daan S <daan.sinke@hotmail.com>
2024-11-11 11:50:55 +0100

    Revert "Merge branch 'Klantenportaal' into layout"
    This reverts commit 5bad6f9115ac24d833fa533a30b02cf151224e4f, reversing
    changes made to 26d51599785e9faaae3575db2e072715cfb9324d.

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\CustomerController;
-
-
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,7 +23,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard/head-marketing', [DashboardController::class, 'headMarketing'])->name('dashboard.head-marketing');
     Route::get('/dashboard/head-maintenance', [DashboardController::class, 'headMaintenance'])->name('dashboard.head-maintenance');
     Route::get('/dashboard/ceo', [DashboardController::class, 'ceo'])->name('dashboard.ceo');
-    Route::resource('customers', CustomerController::class);
 });
 
 Route::middleware('auth')->group(function () {
@@ -36,4 +32,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-

62f284fffef65c65f2428651de6a9ec9ba0d7258
D1L4N0 <dqdebie@gmail.com>
2024-11-14 11:08:24 +0100

    Overal home weggehaald aangezien dit niet meer nodig is omda het logo de home is geworden

--- resources/views/dashboard/head-marketing/head-marketing.blade.php
+++ resources/views/dashboard/head-marketing/head-marketing.blade.php
@@ -6,9 +6,12 @@
         <!-- Sidebar Navigation -->
         <nav class="bg-gray-800 text-white w-50 p-4">
             <ul class="space-y-4">
-                <li class="bg-blue-600 p-2 rounded"><a href="#">Home</a></li>
                 <li class="p-2 rounded hover:bg-gray-700"><a href="{{ route('products.index') }}">Producten</a></li>
-                <li class="p-2 rounded hover:bg-gray-700"><a href="#">Bestellen</a></li>
+                <li class="p-2 rounded hover:bg-gray-700"><a href="#">Offerte maken</a></li>
+                <li class="p-2 rounded hover:bg-gray-700"><a href="#">Offertes</a></li>
+                <li class="p-2 rounded hover:bg-gray-700"><a href="#">Klant registreren</a></li>
+                <li class="p-2 rounded hover:bg-gray-700"><a href="#">Klanten</a></li>
+                <li class="p-2 rounded hover:bg-gray-700"><a href="#">Agenda</a></li>
             </ul>
         </nav>
     </div>

774ccaf5d0b6e421cc9bdcb923536d133c5f5c8e
Daan S <daan.sinke@hotmail.com>
2024-12-12 11:19:02 +0100

    seeder fix

--- database/seeders/QuoteSeeder.php
+++ database/seeders/QuoteSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Quote;
-use App\Models\Customer;
-use App\Models\User;
 use App\Models\Product;
 
 class QuoteSeeder extends Seeder
@@ -31,41 +29,11 @@ class QuoteSeeder extends Seeder
                     ['id' => 2, 'quantity' => 2],
                 ],
                 'beans' => [
-                    ['id' => 5, 'quantity' => 25],
-                    ['id' => 6, 'quantity' => 30],
+                    ['id' => 10, 'quantity' => 25],
+                    ['id' => 11, 'quantity' => 30],
                 ],
             ],
-            [
-                'customer_id' => 2,
-                'user_id' => 3,
-                'status' => 'approved',
-                'quote_date' => now()->subDays(20),
-                'agreement_length' => '24 maanden',
-                'maintenance_agreement' => 'premium',
-                'machines' => [
-                    ['id' => 3, 'quantity' => 1],
-                ],
-                'beans' => [
-                    ['id' => 7, 'quantity' => 20],
-                ],
-            ],
-            [
-                'customer_id' => 3,
-                'user_id' => 3,
-                'status' => 'pending',
-                'quote_date' => now()->subDays(15),
-                'agreement_length' => '12 maanden',
-                'maintenance_agreement' => 'basic',
-                'machines' => [
-                    ['id' => 2, 'quantity' => 1],
-                    ['id' => 4, 'quantity' => 1],
-                ],
-                'beans' => [
-                    ['id' => 6, 'quantity' => 15],
-                    ['id' => 8, 'quantity' => 25],
-                ],
-            ],
-            // Voeg hier de overige 17 offertes in dezelfde structuur toe
+            // Add more quotes here...
         ];
 
         foreach ($quotes as $data) {
@@ -76,26 +44,34 @@ class QuoteSeeder extends Seeder
                 'quote_date' => $data['quote_date'],
                 'agreement_length' => $data['agreement_length'],
                 'maintenance_agreement' => $data['maintenance_agreement'],
-                'total_price' => 0, // Wordt hieronder berekend
+                'total_price' => 0, // Calculated below
             ]);
 
             $totalPrice = 0;
 
-            // Machines koppelen
+            // Link machines
             foreach ($data['machines'] as $machineData) {
-                $machine = $machines->find($machineData['id']);
-                $quote->machines()->attach($machine->id, ['quantity' => $machineData['quantity']]);
-                $totalPrice += $machine->price * $machineData['quantity'];
+                $machine = $machines->firstWhere('id', $machineData['id']);
+                if ($machine) {
+                    $quote->machines()->attach($machine->id, ['quantity' => $machineData['quantity']]);
+                    $totalPrice += $machine->price * $machineData['quantity'];
+                } else {
+                    logger()->warning("Machine with ID {$machineData['id']} not found.");
+                }
             }
 
-            // Bonen koppelen
+            // Link beans
             foreach ($data['beans'] as $beanData) {
-                $bean = $beans->find($beanData['id']);
-                $quote->beans()->attach($bean->id, ['quantity' => $beanData['quantity']]);
-                $totalPrice += $bean->price * $beanData['quantity'];
+                $bean = $beans->firstWhere('id', $beanData['id']);
+                if ($bean) {
+                    $quote->beans()->attach($bean->id, ['quantity' => $beanData['quantity']]);
+                    $totalPrice += $bean->price * $beanData['quantity'];
+                } else {
+                    logger()->warning("Bean with ID {$beanData['id']} not found.");
+                }
             }
 
-            // Totale prijs updaten
+            // Update total price
             $quote->update(['total_price' => $totalPrice]);
         }
     }
